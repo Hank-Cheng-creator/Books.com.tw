ad582349109203e3f3e8f94d1783b48161000757
Hank Cheng <06121122@gm.scu.edu.tw>
2022-05-11 22:48:56 +0800

    Add files via upload

---
+++ 博客來圖片爬取（單頁）.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import requests
+from bs4 import BeautifulSoup
+import shutil
+
+res = requests.get("https://www.books.com.tw/web/sys_bbotm/books/020806/?o=1&v=1&page=1")
+soup = BeautifulSoup(res.text, 'html.parser')
+
+for pictures in soup.select('.cover'):
+    fname = pictures['src'].split('/')[-1].split('&')[0] #取得圖檔名稱
+    res2 = requests.get(pictures['src'].split('i=')[1].split('&')[0], stream=True)
+    pics = open(fname, 'wb') #將取得的圖檔名稱寫入電腦資料夾
+    shutil.copyfileobj(res2.raw, pics)
+    pics.close()
+
